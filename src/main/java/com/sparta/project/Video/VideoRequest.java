aa1e466aacacfdf63ddcc7cd0e2894df170cddaa
HanBeom98 <Carry9691@gmail.com>
2024-08-22 17:22:19 +0900

    s3 동영상 수정4

--- src/main/java/com/sparta/project/Video/VideoRequest.java
+++ src/main/java/com/sparta/project/Video/VideoRequest.java
@@ -3,16 +3,14 @@ package com.sparta.project.Video;
 public class VideoRequest {
     private String title;
     private String description;
-    private String filePath;
 
     // 기본 생성자
     public VideoRequest() {}
 
     // 파라미터 생성자
-    public VideoRequest(String title, String description, String filePath) {
+    public VideoRequest(String title, String description) {
         this.title = title;
         this.description = description;
-        this.filePath = filePath;
     }
 
     // Getter와 Setter
@@ -30,13 +28,5 @@ public class VideoRequest {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
     }
 }
--- src/main/java/com/sparta/project/Video/VideoService.java
+++ src/main/java/com/sparta/project/Video/VideoService.java
@@ -1,13 +1,18 @@
 package com.sparta.project.Video;
 
 import com.sparta.project.s3.S3Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class VideoService {
+
+    private static final Logger logger = LoggerFactory.getLogger(VideoService.class);
 
     @Autowired
     private VideoRepository videoRepository;
@@ -23,7 +28,10 @@ public class VideoService {
         return videoRepository.save(video);
     }
 
+    @Transactional
     public Video saveVideoWithUrl(String title, String description) {
+        logger.info("Starting saveVideoWithUrl method");
+
         // S3에 이미 존재하는 파일의 URL을 가져옴
         String bucketName = "chohanbeom-bucket"; // 실제 S3 버킷 이름
         String keyName = "SACap 2024-06-27 17-20-21-042.mp4"; // S3에 저장된 파일의 키
@@ -32,7 +40,10 @@ public class VideoService {
 
         // URL을 사용해 Video 엔터티를 생성하고 데이터베이스에 저장
         Video video = new Video(title, description, url);
-        return videoRepository.save(video);
+        Video savedVideo = videoRepository.save(video);
+        logger.info("Video saved with ID: " + savedVideo.getId());
+
+        return savedVideo;
     }
 
     public Video findVideoById(Long id) {
